e80f320428b7dffb0286539595765e5bdc5cde68
lljshh <2238715039@qq.com>
2023-03-09 23:52:15 +0800

    Item类的完善

--- Server/game_map.cpp
+++ Server/game_map.cpp
@@ -3,7 +3,27 @@
 int len = 0;
 std::string Itemlist[1 << 16];
 
-Block::Block(){}//区块的构造函数
+void Item::UpdateItem()
+{
+	this->Update(this);
+}
+int Item::PlaceItem()
+{
+	return this->Place(this);
+}
+int Item::DestroyItem()
+{
+	return this->Destroy(this);
+}
+Item::Item(Iupdate _update, Iplace _place, Idestroy _destroy, gmap* _map)
+{
+	this->Update = _update;
+	this->Place = _place;
+	this->Destroy = _destroy;
+	this->Map = _map;
+}
+
+Block::Block(){}//区块的构造函数,需要写生成，seed;
 Block::~Block(){}//区块类的回收
 std::pair<int, int> Block::get_block_id()
 {
--- Server/game_map.h
+++ Server/game_map.h
@@ -1,21 +1,32 @@
 #include <utility>
 #include <map>
 #include <string>
-
+class Item;
+class Entity;
+class Block;
+class gmap;
 using std::make_pair;
 
 extern int len = 0;
-extern std::string Itemlist[1 << 16];
-typedef void (*update)(Item);//更新方块数据
-//template<typename T1, typename T2>
+typedef void (*Iupdate)(Item*);//更新方块数据
+typedef int (*Iplace)(Item*);//放置方块
+typedef int (*Idestroy)(Item*);//破坏方块
 class Item
 {
 public:
-	Item();
+	Item(Iupdate, Iplace, Idestroy, gmap*);
 	~Item();
+	std::map<std::string, std::string> date;
+	void UpdateItem();
+	int PlaceItem();
+	int DestroyItem();
+	gmap* Map;
 private:
 	int id;
 	std::pair<int, int> address;
+	Iupdate Update;
+	Iplace Place;
+	Idestroy Destroy;
 };
 class Block
 {
@@ -28,8 +39,9 @@ private:
 	Item m[256][256];//区块的信息
 	int x, y;//区块的编号
 };
-struct gmap//地图结构，没有复杂的功能需实现，使用后结构体
+class gmap//地图结构，没有复杂的功能需实现，使用后结构体
 {
+public:
 	std::map<std::pair<int, int>, Block> blocks;
 	Item get_item(int x, int y)
 	{
