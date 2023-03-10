8188021c4cdd8e024863d26784ba95efc3646259
lljshh <2238715039@qq.com>
2023-03-10 22:29:38 +0800

    22:30提交

--- Server/game_map.cpp
+++ Server/game_map.cpp
@@ -23,8 +23,18 @@ Item::Item(Iupdate _update, Iplace _place, Idestroy _destroy, gmap* _map, std::s
 	this->Map = _map;
 	this->id = _id;
 }
+Item::Item(Sitem _item, gmap *_map)
+{
+	this->Update = _item._update;
+	this->Place = _item._place;
+	this->Destroy = _item._destroy;
+	this->Map = _map;
+	this->id = _item._id;
+}
+Item::~Item() {};
 Addres Item::get_addres() { return this->addres; }
-
+void Item::set_addres(Addres _addres) { this->addres = _addres };
+//实体类
 Block::Block(){}//区块的构造函数,需要写生成，seed;
 Block::~Block(){}//区块类的回收
 std::pair<int, int> Block::get_block_id()
--- Server/game_map.h
+++ Server/game_map.h
@@ -21,25 +21,69 @@ typedef void (*Eupdate)(Entity*);//更新实体数据
 typedef int (*Esummon)(Entity*);//生成实体
 typedef int (*Ekill)(Entity*);//杀死实体
 //一些辅助struct的声明与定义
-struct Sitem
+struct Sitem//item 类的值
 {
-
+	Iupdate _update;
+	Iplace _place;
+	Idestroy _destroy;
+	std::string _id;
+};
+struct Sentity
+{
+	Eupdate _update;
+	Esummon _summon;
+	Ekill _kill;
+	std::string _id;
+};
+template<typename T>
+struct GList
+{
+	T pool[100];
+	int now = 0;
+	_list first;
+	GList(T* me)
+	{
+		this->first.prev = NULL;
+		this->first.next = NULL;
+		this->first.me = me;
+	}
+};
+struct _list
+{
+	_list* prev;
+	_list* next;
+	void* me;
+	void del()//删完记得delete这个家伙
+	{
+		prev->next = next;
+		next->prev = prev;
+		delete me;
+	}
+	void add(_list* newone)//在后面加
+	{
+		_list* lnext = next;
+		next = newone;
+		newone->prev = this;
+		newone->next = lnext;
+	}
 };
 //全局变量的声明，记得加extern并且在同名的.cpp中定义
 extern int Ilength = 0;//方块数量
 extern Sitem items[1000];
 extern int Elength = 0;//实体数量
-//让后就是类的声明了
+//然后就是类的声明了
 class Item//方块类
 {
 public:
 	Item(Iupdate, Iplace, Idestroy, gmap*, std::string);
+	Item(Sitem, gmap*);
 	~Item();
 	std::map<std::string, std::string> date;
 	void UpdateItem();
 	int PlaceItem();
 	int DestroyItem();
 	Addres get_addres();
+	void set_addres(Addres);
 	gmap* Map;
 	std::string id;
 private:
@@ -63,8 +107,8 @@ public:
 private:
 	Addres addres;
 	Eupdate Update;
-	Esummon Place;
-	Ekill Destroy;
+	Esummon Summon;
+	Ekill Kill;
 };
 class Block//区块类，为了大地图的生成及省空间
 {
